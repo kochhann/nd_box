112d9aa2f45083df2f6a82f070303239722e4fcd
Juliano Kochhann <juliano@kochhann.com.br>
2023-01-13 16:58:21 -0300

    first commit on hml

---
+++ nd_box/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+
+
+urlpatterns = [
+    path('', include('apps.core.urls')),
+    path('manager/', admin.site.urls),
+    path('carreiras/', include('apps.carreiras.urls')),
+    path('reports/', include('apps.reports.urls')),
+    path('privacidade/', include('apps.privacidade.urls')),
+    path('autorizacoes/', include('apps.autorizacoes.urls')),
+    path('jund/', include('apps.jund.urls')),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+admin.site.site_header = 'Gestão ND Box'
+admin.site.site_title = 'Gestão ND Box'
+admin.site.index_title = 'Área Administrativa'
